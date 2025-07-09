6095913eded3cd4e406b71e48cbc595ecfb21c86
maxhu08 <contact@maxhu.dev>
2025-07-09 16:25:26 -0700

    fix issue where an extra bed is created when merging

--- bed_level_1.gd
+++ bed_level_1.gd
@@ -2,9 +2,12 @@
 extends RigidBody3D
 
 @export var bed_color: Color = Color(0, 1, 0)
+@export var level: int = 1
 
 @onready var mesh = $MeshInstance3D
 @onready var bed_level2_scene = preload("res://bed_level_2.tscn")
+
+var combined = false
 
 func _ready():
 	if mesh.mesh == null:
@@ -27,12 +30,18 @@ func _update_color():
 	mesh.material_override = mat
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if combined:
+		return
+
 	for i in range(state.get_contact_count()):
 		var collider = state.get_contact_collider_object(i)
 		if collider and collider is RigidBody3D and collider.get_script() == self.get_script():
-			var new_bed = bed_level2_scene.instantiate()
-			new_bed.global_transform.origin = (global_transform.origin + collider.global_transform.origin) * 0.5
-			get_tree().current_scene.add_child(new_bed)
-			queue_free()
-			collider.queue_free()
-			break
+			if level == 1 and collider.level == 1:
+				combined = true
+				collider.combined = true
+				var new_bed = bed_level2_scene.instantiate()
+				new_bed.global_transform.origin = (global_transform.origin + collider.global_transform.origin) * 0.5
+				get_tree().current_scene.add_child(new_bed)
+				queue_free()
+				collider.queue_free()
+				break
--- dropper.gd
+++ dropper.gd
@@ -19,7 +19,7 @@ extends Node3D
 		_update_dropper_position()
 
 @onready var object_to_spawn = preload("res://bed_level_1.tscn")
-var rigid_body: RigidBody3D
+var character_body: CharacterBody3D
 
 func _ready():
 	if not Engine.is_editor_hint():
@@ -27,51 +27,52 @@ func _ready():
 	_create_body()
 
 func _update_dropper_position():
-	if rigid_body:
-		rigid_body.global_transform.origin = dropper_position
+	if character_body:
+		character_body.global_transform.origin = dropper_position
 
 func _input(event):
-	if event.is_action_pressed("click") and rigid_body:
-		var spawn_position = rigid_body.global_transform.origin - Vector3(0, size.y / 2 + 0.1, 0)
+	if event.is_action_pressed("click") and character_body:
+		var spawn_position = character_body.global_transform.origin - Vector3(0, 3, 0)
 		var instance = object_to_spawn.instantiate()
 		instance.global_transform.origin = spawn_position
 		get_tree().current_scene.add_child(instance)
 
 func _physics_process(delta):
-	if Engine.is_editor_hint() or not rigid_body:
+	if Engine.is_editor_hint() or not character_body:
 		return
 
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("move_forward"):
-		direction -= rigid_body.transform.basis.z
+		direction -= character_body.transform.basis.z
 	if Input.is_action_pressed("move_back"):
-		direction += rigid_body.transform.basis.z
+		direction += character_body.transform.basis.z
 	if Input.is_action_pressed("move_left"):
-		direction -= rigid_body.transform.basis.x
+		direction -= character_body.transform.basis.x
 	if Input.is_action_pressed("move_right"):
-		direction += rigid_body.transform.basis.x
+		direction += character_body.transform.basis.x
 
 	direction.y = 0
 	direction = direction.normalized()
 
-	rigid_body.linear_velocity.x = direction.x * speed
-	rigid_body.linear_velocity.z = direction.z * speed
-	rigid_body.linear_velocity.y = 0
+	var velocity = direction * speed
+
+	character_body.velocity.x = velocity.x
+	character_body.velocity.z = velocity.z
+	character_body.velocity.y = 0
+
+	character_body.move_and_slide()
 
 func _create_body():
 	for child in get_children():
-		if child is RigidBody3D:
+		if child is CharacterBody3D:
 			remove_child(child)
 			child.queue_free()
 
-	rigid_body = RigidBody3D.new()
-	rigid_body.name = "DropperRigidBody"
-	rigid_body.gravity_scale = 0
-	rigid_body.collision_layer = 1
-	rigid_body.collision_mask = 1
-	add_child(rigid_body)
-	rigid_body.owner = self.get_owner()
+	character_body = CharacterBody3D.new()
+	character_body.name = "DropperCharacterBody"
+	add_child(character_body)
+	character_body.owner = self.get_owner()
 
 	_update_dropper_position()
 
@@ -80,14 +81,14 @@ func _create_body():
 	box_shape.size = size
 	collision.shape = box_shape
 	collision.disabled = false
-	rigid_body.add_child(collision)
+	character_body.add_child(collision)
 	collision.owner = self.get_owner()
 
 	var mesh = MeshInstance3D.new()
 	var box_mesh = BoxMesh.new()
 	box_mesh.size = size
 	mesh.mesh = box_mesh
-	rigid_body.add_child(mesh)
+	character_body.add_child(mesh)
 	mesh.owner = self.get_owner()
 
 	var mat = StandardMaterial3D.new()
@@ -95,10 +96,10 @@ func _create_body():
 	mesh.material_override = mat
 
 func _update_body_color():
-	if not rigid_body:
+	if not character_body:
 		return
 
-	for child in rigid_body.get_children():
+	for child in character_body.get_children():
 		if child is MeshInstance3D:
 			var mat = child.material_override
 			if mat:
