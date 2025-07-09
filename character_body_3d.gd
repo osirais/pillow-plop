260a4faf7ae3e68fc59e3e129ef9274ba5ff730a
maxhu08 <contact@maxhu.dev>
2025-07-08 19:02:23 -0700

    add moveable player

---
+++ character_body_3d.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+const SPEED := 5.0
+const MOUSE_SENSITIVITY := 0.002
+
+@onready var camera = $Camera3D
+var yaw = 0.0
+var pitch = 0.0
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		yaw -= event.relative.x * MOUSE_SENSITIVITY
+		pitch -= event.relative.y * MOUSE_SENSITIVITY
+		pitch = clamp(pitch, -PI / 2, PI / 2)
+
+		rotation.y = yaw
+		camera.rotation.x = pitch
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z
+	if Input.is_action_pressed("move_back"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+
+	direction = direction.normalized()
+	velocity = direction * SPEED
+	move_and_slide()
